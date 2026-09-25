9cddb79bc5c8c6f5139d8adde967d11a6fe975d0
Amitanshu Varshney <amitanshu.varshney@gmail.com>
2026-09-26 01:17:18 +0530

    Time: 3 ms (50.31%), Space: 46.3 MB (78.92%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+
+        for(int i = 0 ; i < ransomNote.length() ; i++){
+            char ch = ransomNote.charAt(i);
+            arr1[ch - 'a']++;
+        }
+
+        for(int i = 0 ; i < magazine.length() ; i++){
+            char ch = magazine.charAt(i);
+            arr2[ch - 'a']++;
+        }
+
+        for(int i = 0 ; i < 26 ; i++){
+            if(arr1[i] <= arr2[i]){
+
+            }
+            else{
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
